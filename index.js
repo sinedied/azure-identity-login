47eb89325747fff81726d6a9c41e5c5278e85722
Wassim Chegham <github@wassim.dev>
2022-03-29 15:14:41 +0200

    chore: make persistence optional

--- index.js
+++ index.js
@@ -1,38 +1,57 @@
-
-const { DeviceCodeCredential, InteractiveBrowserCredential, ChainedTokenCredential, useIdentityPlugin } = require("@azure/identity");
+const {
+  DeviceCodeCredential,
+  InteractiveBrowserCredential,
+  ChainedTokenCredential,
+  useIdentityPlugin,
+} = require("@azure/identity");
 const { cachePersistencePlugin } = require("@azure/identity-cache-persistence");
+const { vsCodePlugin } = require("@azure/identity-vscode");
 const { SubscriptionClient } = require("@azure/arm-subscriptions");
 const { WebSiteManagementClient } = require("@azure/arm-appservice");
 
-const tokenCachePersistenceOptions = {
+let tokenCachePersistenceOptions = {
+  enabled: false,
   name: "identity.cache",
-  enabled: true,
-  // unsafeAllowUnencryptedStorage: true
-}
+  // avoid error: Unable to read from the system keyring (libsecret).
+  unsafeAllowUnencryptedStorage: true,
+};
 
-useIdentityPlugin(cachePersistencePlugin);
+if (process.env.AZURE_IDENTITY_ENABLE_CACHE_PERSISTENCE) {
+  useIdentityPlugin(cachePersistencePlugin);
+  useIdentityPlugin(vsCodePlugin);
+  tokenCachePersistenceOptions.enabled = true;
+}
 
 async function login() {
   const browserCredential = new InteractiveBrowserCredential({
-    redirectUri: "http://localhost:1337",
-    tokenCachePersistenceOptions
+    redirectUri: "http://localhost:8888",
+    tokenCachePersistenceOptions,
   });
   const deviceCredential = new DeviceCodeCredential({
-    tokenCachePersistenceOptions
+    tokenCachePersistenceOptions,
   });
-  const credentialChain = new ChainedTokenCredential(browserCredential, deviceCredential);
-  
-  const subscriptionClient = new SubscriptionClient(credentialChain);
-  const subscriptions = await subscriptionClient.subscriptions.list();
+  const credentialChain = new ChainedTokenCredential(
+    browserCredential,
+    deviceCredential
+  );
 
-  for await (let subscription of subscriptions) {
-    console.log(`Static web apps in subscription ${subscription.displayName}:`);
-    const websiteClient = new WebSiteManagementClient(credentialChain, subscription.subscriptionId);
-    const staticSites = await websiteClient.staticSites.list();
-    for await (let site of staticSites) {
+  const subscriptionClient = new SubscriptionClient(credentialChain);
+
+  for await (let subscription of subscriptionClient.subscriptions.list()) {
+    console.log(
+      `Static web apps in subscription ${subscription.displayName} (${subscription.id}):`
+    );
+    const websiteClient = new WebSiteManagementClient(
+      credentialChain,
+      subscription.subscriptionId
+    );
+
+    for await (let site of websiteClient.staticSites.list()) {
       console.log(`- ${site.name} (${site.id})`);
     }
   }
 }
 
-login();
+(async () => {
+  await login();
+})();
